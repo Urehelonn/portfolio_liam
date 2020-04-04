2560644d10e0e285c25e4423479afdadac5f16a9
Liam <urehelonn@gmail.com>
2020-04-03 23:03:01 -0400

    header change
    toggle need to be fixed

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -4,50 +4,42 @@ import { Nav, NavDropdown, Navbar } from 'react-bootstrap';
 import './header.style.scss';
 
 const Header = () => {
-    const [activeKey, setActiveKey] = useState('1');
+    const [activeKey, setActiveKey] = useState(1);
     const handleSelect = (eventKey) => {
         setActiveKey(eventKey);
-        // state.activeKey = eventKey;
         console.log(activeKey);
     };
 
+    const [expanded, setExpanded] = useState(false);
+
     return (
-        <Nav variant="pills" activeKey={activeKey} onSelect={handleSelect} >
-            <Navbar.Brand>
-                {/* <img src="./../../images/logo.png" width="35px" height="30px" /> */}
+        <Nav className='navbar navbar-expand-lg navbar-dark bg-dark static-top'
+            variant="pills" activeKey={activeKey} onSelect={handleSelect}>
+            <Navbar.Brand className='navbar-brand'>
                 <Link to={'/'}>Liam's Work Space</Link>
             </Navbar.Brand>
 
-            <Nav.Item>
-                <Nav.Link as={Link} to="/" eventKey={1} title="Item">
-                    Main Page
-                </Nav.Link>
-            </Nav.Item>
-
-            <Nav.Item>
-                <Nav.Link as={Link} to="/about" eventKey={1} title="Item">
-                    About
-                </Nav.Link>
-            </Nav.Item>
-
-            <Nav.Item>
-                <Nav.Link as={Link} to="/contact" eventKey={1} title="Item">
-                    Contact
-                </Nav.Link>
-            </Nav.Item>
-
-            {/* <Nav.Item>
-                <Nav.Link eventKey={2} title="Item">
-                    Contact
-                </Nav.Link>
-            </Nav.Item> */}
-            {/* <NavDropdown title="Dropdown" id="nav-dropdown">
-                <NavDropdown.Item eventKey={4.1}>Action</NavDropdown.Item>
-                <NavDropdown.Item eventKey={4.2}>Another action</NavDropdown.Item>
-                <NavDropdown.Item eventKey={4.3}>Something else here</NavDropdown.Item>
-                <NavDropdown.Divider />
-                <NavDropdown.Item eventKey="4.4">Separated link</NavDropdown.Item>
-            </NavDropdown> */}
+            <Navbar.Toggle onClick={() => setExpanded(expanded ? false : "expanded")}
+                aria-controls="basic-navbar-nav" />
+            <Navbar.Collapse id="basic-navbar-nav" className="justify-content-end">
+                <Nav onClick={handleSelect}>
+                    <Nav.Item>
+                        <Nav.Link as={Link} to="/" eventKey={1} title="Item">
+                            Main Page
+                        </Nav.Link>
+                    </Nav.Item>
+                    <Nav.Item>
+                        <Nav.Link as={Link} to="/about" eventKey={2} title="Item">
+                            About
+                        </Nav.Link>
+                    </Nav.Item>
+                    <Nav.Item>
+                        <Nav.Link as={Link} to="/contact" eventKey={3} title="Item">
+                            Contact
+                        </Nav.Link>
+                    </Nav.Item>
+                </Nav>
+            </Navbar.Collapse>
         </Nav>
     );
 }
