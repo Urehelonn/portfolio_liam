904c2f6e8b8bd017e4c68d30559b518cd34b0352
Liam <urehelonn@gmail.com>
2023-08-02 23:32:29 -0400

    apps redirect

--- src/pages/apps/AppsPage.tsx
+++ src/pages/apps/AppsPage.tsx
@@ -1,6 +1,12 @@
 import NavBar from "@/components/navbar";
+import {useEffect} from "react";
+import {useRouter} from "next/router";
 
 const AppsPage = () => {
+    const router = useRouter();
+    useEffect(() => {
+        router.push('/underConstruction');
+    }, [])
     return (<div><h2>Apps</h2>
         <hr/>
         <NavBar/>
