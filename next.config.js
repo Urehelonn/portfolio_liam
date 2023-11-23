9876364d617798840c412fdfc5abd29c316cbc31
Cavy Cavy <urehelonn@gmail.com>
2023-11-23 00:54:05 -0500

    Update next.config.js
    export setting change and colour clear fix
    
    Update MountainAnimationDiv.tsx

--- next.config.js
+++ next.config.js
@@ -1,4 +1,6 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  output: "standalone", // You can change 'out' to the desired output directory
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
