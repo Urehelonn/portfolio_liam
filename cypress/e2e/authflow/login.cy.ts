be15f04d25f0fdb440a09d3d5bd49c3a3eaf3a6f
Liam <urehelonn@gmail.com>
2024-01-30 02:12:47 -0500

    login case added

--- cypress/e2e/authflow/login.cy.ts
+++ cypress/e2e/authflow/login.cy.ts
@@ -1,6 +1,9 @@
 import { generateRandomString } from '../../support/common';
 
 describe('Login page test', () => {
+  let usedUsername: string;
+  let usedPassword: string;
+
   beforeEach(() => {
     cy.visit('/login');
   });
@@ -40,7 +43,10 @@ describe('Login page test', () => {
     cy.get('#password-helper-text')
       .should('exist')
       .should('be.visible')
-      .should('have.text', 'Need to have 6 or more digit of mixed number and letters.')
+      .should(
+        'have.text',
+        'Need to have 6 or more digit of mixed number and letters.'
+      )
       .should('have.class', 'Mui-error');
     cy.contains('button', /SIGN IN/i).should(
       'not.have.attr',
@@ -51,13 +57,15 @@ describe('Login page test', () => {
 
   it('should create proper request and response of 201 CREATED and nav to main page', () => {
     cy.visit('/register');
-    const email = generateRandomString(5) + '@test.ca';
-    const password = 'wasd1wasd';
+    usedUsername = generateRandomString(5) + '@test.ca';
+    usedPassword = 'wasd1wasd';
     cy.get('#passwordConfirmation').click();
-    cy.get('#username').clear().type(email);
-    cy.get('#password').type(password);
-    cy.get('#passwordConfirmation').type(password);
-    cy.intercept('POST', Cypress.env('backendUrl') + '/user/register').as('register');
+    cy.get('#username').clear().type(usedUsername);
+    cy.get('#password').type(usedPassword);
+    cy.get('#passwordConfirmation').type(usedPassword);
+    cy.intercept('POST', Cypress.env('backendUrl') + '/user/register').as(
+      'register'
+    );
     cy.contains('button', /SIGN UP/i).click();
     cy.get('@register', { timeout: 5000 }).should((interception) => {
       expect(interception).to.have.property('response');
@@ -67,15 +75,29 @@ describe('Login page test', () => {
     });
 
     cy.visit('/login');
-    cy.get('#email').clear().type(email);
-    cy.get('#password').type(password);
+    cy.get('#email').clear().type(usedUsername);
+    cy.get('#password').type(usedPassword);
 
-    cy.intercept('POST', Cypress.env('backendUrl') + '/user/login').as(
-      'login'
-    );
-    cy.contains('button', /SIGN IN/i).click()
+    cy.intercept('POST', Cypress.env('backendUrl') + '/user/login').as('login');
+    cy.contains('button', /SIGN IN/i).click();
 
-    cy.wait(2000)
+    cy.wait(2000);
     cy.get('h1').invoke('text').should('include', 'Liam');
+  });
+
+  it('should create proper request and response of 400 BAD REQUEST', () => {
+    cy.get('#email').clear().type(usedUsername);
+    cy.get('#password').type('wasdasd');
+
+    cy.intercept('POST', Cypress.env('backendUrl') + '/user/login').as('login');
+    cy.contains('button', /SIGN IN/i).click();
+
+    cy.get('@login', { timeout: 5000 }).should((interception) => {
+      expect(interception).to.have.property('response');
+    });
+    cy.on('window:alert', (alertText) => {
+      expect(alertText).to.equal('UserExceptions: Username and password combination not found.');
+    });
+
   });
 });
